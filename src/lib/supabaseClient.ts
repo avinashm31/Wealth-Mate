6e6ce8df0089ae6d203877e6cd9a71a351f3bed7
avinashm31 <tbm26avinash.murali@mastersunion.org>
2025-11-26 14:34:42 +0530

    Delete src directory

--- src/lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/supabaseClient.ts
-import { createClient } from "@supabase/supabase-js";
-import { ENV } from "../config/env";
-
-export const supabase = createClient(
-  ENV.SUPABASE_URL,
-  ENV.SUPABASE_ANON_KEY
-);
